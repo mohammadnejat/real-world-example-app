3ca0966b65fc83ce953d4d53ce5b7c8284fecdb9
MohammadNejat01 <mohammadofahmad@gmail.com>
2025-10-29 00:01:39 +0330

    add routerlink

--- src/app/features/profile/profile.ts
+++ src/app/features/profile/profile.ts
@@ -4,10 +4,11 @@ import { ProfileStore } from './store/profile.store';
 import { Articles } from '../../core/services/articles';
 import { Profiles } from '../../core/services/profiles';
 import { DatePipe } from '@angular/common';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-profile',
-  imports: [DatePipe],
+  imports: [DatePipe, RouterLink],
   templateUrl: './profile.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [ProfileStore,Articles,Profiles],
